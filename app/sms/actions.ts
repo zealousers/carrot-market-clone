5d5bf32a6e8375e7ccb95214a4cb0946a87d02f9
zealousers <triangleone@naver.com>
2024-04-16 21:40:08 +0900

    4.8 SMS Validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -2,12 +2,40 @@
 
 import {z}from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z.string().trim().refine((phone)=>validator.isMobilePhone(phone,"ko-KR"),"Wrong phone format");
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogin(prevState:any,formData:FormData){
-  console.log(typeof formData.get("phone"));
-  console.log(typeof tokenSchema.parse(formData.get("token")));
+interface ActionState{ 
+  token:boolean;
+}
+
+export async function smsLogin(prevState:ActionState,formData:FormData){
+  // console.log(typeof formData.get("phone"));
+  // console.log(typeof tokenSchema.parse(formData.get("token")));
+  const phone = formData.get("phone");
+  const token = formData.get('token');
+  if(!prevState.token){
+    const result=phoneSchema.safeParse(phone);
+    if(!result.success){
+      return{token:false,
+        error:result.error.flatten()
+      };
+    }else {
+      return{token:true};
+    }
+  } else{
+    const result=tokenSchema.safeParse(token);
+    if(!result.success){
+      return{
+        token:true,
+        error:result.error.flatten()
+      };
+    }else {
+      // return{token:false};
+      redirect("/");
+    }
+  }
   
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -4,8 +4,13 @@ import Button from "@/components/button";
 import { useFormState } from "react-dom";
 import { smsLogin } from "./actions";
 
+const initialState = {
+  token: false,
+  error: undefined,
+};
+
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsLogin, null);
+  const [state, dispatch] = useFormState(smsLogin, initialState);
   return (
     <div className='flex flex-col gap-10 py-8 px-6'>
       <div className='flex flex-col gap-2 *:font-medium'>
@@ -13,18 +18,27 @@ export default function SMSLogin() {
         <h2 className='text-xl'>Verify your phone number.</h2>
       </div>
       <form action={dispatch} className='flex flex-col gap-3'>
-        <Input name='phone' type='number' placeholder='Phone number' required={true} errors={[]} />
-        <Input
-          name='token'
-          type='number'
-          placeholder='Verification code'
-          required={true}
-          errors={[]}
-          min={100000}
-          max={999999}
-        />
+        {state.token ? (
+          <Input
+            name='token'
+            type='number'
+            placeholder='Verification code'
+            required={true}
+            errors={[]}
+            min={100000}
+            max={999999}
+          />
+        ) : (
+          <Input
+            name='phone'
+            type='text'
+            placeholder='Phone number'
+            required={true}
+            errors={state.error?.formErrors}
+          />
+        )}
 
-        <Button text='Verify' />
+        <Button text={state.token ? "Verify Token" : "Send Verification SMS"} />
       </form>
     </div>
   );
