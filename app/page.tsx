1af68016994d137cef8a01b6908d7bf403e58bbf
zealousers <triangleone@naver.com>
2024-04-17 15:42:22 +0900

    #7.2 Prisma Client (06:07)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import "@/lib/db";
+// import "@/lib/db";
 
 export default function Home() {
   return (
--- lib/db.ts
+++ lib/db.ts
@@ -2,9 +2,9 @@ import {PrismaClient} from '@prisma/client'
 
 const db = new PrismaClient();
 
-async function main() {
-  const user = await db.user.create({data:{username:"alice",}});
-  console.log(user);}
+// async function main() {
+//   const user = await db.user.create({data:{username:"alice",}});
+//   console.log(user);}
 
-  main()
+//   main()
 export default db;
